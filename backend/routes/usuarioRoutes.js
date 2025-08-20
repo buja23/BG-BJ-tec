d6985a392e5612c7f4451c7dca26e1a7b45e6c25
buja23 <jooj.soares.227@gmail.com>
2025-08-20 17:27:35 -0300

    login e cadastro

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,26 +1,15 @@
-// routes/usuarioRoutes.js
 import express from 'express';
 import controle from '../controllers/UsuarioController.js';
 
 const router = express.Router();
 
-// POST   /api/usuarios      → cria um usuário
 router.post('/usuario', controle.create);
-
-// GET    /api/usuarios      → lista todos
 router.get('/usuario', controle.list);
+router.get('/usuario/:id', controle.getById);
+router.put('/usuario/:id', controle.update);
+router.delete('/usuario/:id', controle.remove);
 
-// GET    /api/usuarios/:id  → busca por ID
-router.get('/usuario:id', controle.getById);
-
-// PUT    /api/usuarios/:id  → atualiza usuário
-router.put('/usuario:id', controle.update);
-
-// DELETE /api/usuarios/:id  → remove usuário
-router.delete('/usuario:id', controle.remove);
-
-router.get('/cadastrar', (req, res) => {
-    res.render('cadastrarUsuario');
-  });
+// 🚀 Rota de login
+router.post('/login', controle.login);
 
 export default router;
