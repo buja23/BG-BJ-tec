9577ff978ec426b2986c4773d451de34ab9d2d1f
buja23 <jooj.soares.227@gmail.com>
2025-09-30 23:22:39 -0300

    "cadastro"

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import ThemeToggle from '../components/ThemeToggle';
-import './Cadastro.css';
+import '../cadastro.css';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
